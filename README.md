6840f1103686f429b9777b8eed3f3136dd9ab830
pcfaktor <pcfaktor@gmail.com>
2023-08-24 13:05:13 +0300

    feat: implement models

---
+++ README.md
@@ -0,0 +1,12 @@
+# CR8S
+
+### A simple Rust crates registry application
+
+## Overview
+
+* Hyper
+* Rocket
+* Diesel
+* Redis
+
+
---
+++ migrations/2023-08-24-081405_create_rustaceans/up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE rustaceans (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    crated_at TIMESTAMP DEFAULT NOW() NOT NULL
+)
